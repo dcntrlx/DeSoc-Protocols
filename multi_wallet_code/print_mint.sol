af1af6080c9e25b156d56728b23a1991171c51b4
dcntrl <dcntrlx@gmail.com>
2025-12-04 23:04:28 +0300

    Refactoring of repository

--- multi_wallet_code/print_mint.sol
+++ multi_wallet_code/print_mint.sol
@@ -2,21 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract P {
-
     address[] public owners;
     address public o;
 
     event D(address da);
-    
+
     constructor() {
-        
         o = msg.sender;
 
         emit D(o);
-
     }
-
-
-
-
-} 
+}
